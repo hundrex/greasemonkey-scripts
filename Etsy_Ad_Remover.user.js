4de693baec762785bcbad70b7579732586f9a3b9
Samuel Taylor <github@samueltaylor.org>
2016-01-29 15:59:58 -0600

    Fix Etsy search ad remover

--- Etsy_Ad_Remover.user.js
+++ Etsy_Ad_Remover.user.js
@@ -3,8 +3,14 @@
 // @namespace   http://www.samueltaylor.org/
 // @description Removes search ads from Etsy
 // @include     https://www.etsy.com/search*
-// @version     1.0
+// @version     1.1
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant       GM_addStyle
 // ==/UserScript==
 
-$(".ad-indicator").parent().remove();
+waitForKeyElements (".ad", actionFunction);
+
+function actionFunction (jNode) {
+    jNode.parent().parent().remove();
+}
