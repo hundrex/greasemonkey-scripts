b81a4f000c77a622940011b1a458601f8fcf7b82
Samuel Taylor <github@samueltaylor.org>
2016-11-24 07:36:16 -0800

    Fix Pocket ad remover

--- Pocket_Feed_Ad_Remover.user.js
+++ Pocket_Feed_Ad_Remover.user.js
@@ -2,16 +2,16 @@
 // @name        Pocket Feed Ad Remover
 // @namespace   http://www.samueltaylor.org/
 // @description Removes feed ads on Pocket
-// @include     https://www.getpocket.com/a/queue/list/
-// @include     http://www.getpocket.com/a/queue/list/
-// @version     1.0
+// @include     https://getpocket.com/a/queue/list/
+// @include     http://getpocket.com/a/queue/list/
+// @version     1.1
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant       GM_addStyle
 // ==/UserScript==
 
-waitForKeyElements (".item_type_spoc", actionFunction);
+waitForKeyElements(".item_type_spoc", actionFunction, false);
 
-function actionFunction (jNode) {
+function actionFunction(jNode) {
     jNode.remove();
 }
