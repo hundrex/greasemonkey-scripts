fd243e4d60f6e0a9b5a47c642fe202e0175f5aa7
Samuel Taylor <github@samueltaylor.org>
2016-11-24 07:36:47 -0800

    Update Value Remover

--- Value_Remover.user.js
+++ Value_Remover.user.js
@@ -3,13 +3,14 @@
 // @namespace   http://www.samueltaylor.org/
 // @description Removes search ads from L'Occitane's website
 // @include     http://usa.loccitane.com/*
-// @version     0.9
+// @version     0.91
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant       GM_addStyle
 // ==/UserScript==
 
 waitForKeyElements(".productValue", actionFunction);
+waitForKeyElements(".product_price_value", actionFunction);
 
 function actionFunction(jNode) {
     jNode.remove();
