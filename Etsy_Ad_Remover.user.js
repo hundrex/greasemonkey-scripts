0c9a4e1d9e0267534494c063ccbed4228a24f35c
Chris Stewart <hostage@users.noreply.github.com>
2019-09-05 12:09:26 -0700

    Update Etsy_Ad_Remover.user.js
    Per https://github.com/ssaamm/greasemonkey-scripts/issues/2
    Note: Also changed the include filter, since lots of places link via just etsy.com (no www) and /market also has ads ...

--- Etsy_Ad_Remover.user.js
+++ Etsy_Ad_Remover.user.js
@@ -2,15 +2,15 @@
 // @name        Etsy Ad Remover
 // @namespace   http://www.samueltaylor.org/
 // @description Removes search ads from Etsy
-// @include     https://www.etsy.com/search*
+// @include     https://*.etsy.com/*
 // @version     1.1
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant       GM_addStyle
 // ==/UserScript==
 
-waitForKeyElements (".ad", actionFunction);
+waitForKeyElements (".ad-iindicator", actionFunction);
 
 function actionFunction (jNode) {
-    jNode.parent().parent().remove();
+jNode.parent().parent().parent().parent().parent().parent().parent().remove();
 }
